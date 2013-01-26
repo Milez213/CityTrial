5cd4ba36db551ded9ad93529e39660459f3a5b63
krisko6 <cpauley@calpoly.edu>
2013-01-26 14:16:41 -0800

    Update KPPObject.h

--- KPPObject.h
+++ KPPObject.h
@@ -1,10 +1,17 @@
-
 /* PhysX Object */
 
 class KPPObject {
 	protected:
-		vec3 velocity();
+		glm::vec3 velocity();
 		void set_velocity(vec3 vel);
+		glm::vec3 position();
+		void set_position(vec3 p);
+		glm::vec3 rotation();
+		void set_rotation(vec3 r);
+		glm::vec3 scale();
+		void set_scale(vec3 s);
+		//Bounding Box?
+		//Bounding sphere?
 		/* Getters and Setters */
 		/* Velocity - vec3
 		 * Position - vec3
@@ -17,24 +24,42 @@ class KPPObject {
 
 class KPPDrawnObject : public KPPObject {
 	protected:
+	         GLuint meshIndex();
+	         void meshIndex(GLunint i);
+	         GLuint textureIndex();
+	         void textureIndex(GLuint i);
+	         glm::vec3 diffColor();
+	         void diffColor(glm::vec3 c);
+	         glm::vec3 specColor();
+	         void specColor(glm::vec3 c);
+	         float specularity();
+	         void specularity(float f);
+	         void draw();
+	         //Properties??
 		/* Getters and Setters */
 		/* MeshIndex - GLuint
 		 * TextureIndex - GLuint
 		 * DiffColor - vec3
 		 * SpecColor - vec3
 		 * Specularity - float
-		 * proporties - struct
+		 * properties - struct
 		 */
 };
 
 class KPPCamera : public KPPObject {
 	protected:
+	        glm::vec3 lookAt();
+	        void lookAt(glm::vec3 l);
 		/* Getters and Setters */
 		/* LookAt - vec3 */	
 };
 
 class KPPLight : public KPPObject {
 	protected:
+	        glm::vec3 color();
+	        void color(glm::vec3 c);
+	        glm::vec3 intensity();
+	        void intensity(glm::vec3 i);
 		/* Getters and Setters */
 		/* Color - vec3 
 		 * Intensity - float */	
