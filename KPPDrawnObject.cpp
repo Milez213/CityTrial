9d9218aca0b35a3c74f09c3d921b25ececa85088
krisko6 <cpauley@calpoly.edu>
2013-01-27 21:58:10 -0800

    Create KPPDrawnObject.cpp

---
+++ KPPDrawnObject.cpp
@@ -0,0 +1,23 @@
+#include "KPPDrawnObject.h"
+
+class KPPDrawnObject : public KPPObject {
+
+public: 
+KPPDrawnObject(KPPPhysics *physics,const char *meshFile){
+   
+}
+
+protected:
+
+/*GLuint meshIndex;
+GLunint textureIndex;
+glm::vec3 diffColor;
+glm::vec3 specColor;
+float specularity;
+KPPPhysics *p;
+*/
+
+void draw(/*shaders*/){
+}
+
+};
