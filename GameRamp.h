5ad0091bbbf1ad5bdee800c86cf55b6a4590e878
Bryan Bell <bbell02@calpoly.edu>
2013-01-30 08:17:37 -0800

    Updated Ramp

--- GameRamp.h
+++ GameRamp.h
@@ -4,7 +4,8 @@
 //
 //  Created by Bryan Bell on 1/30/13.
 //
-//
+// NOTE: Do not rotate this object by anything but the y-axis,
+//  it will not work properly if the object is not flat.
 
 #ifndef ____GameRamp__
 #define ____GameRamp__
