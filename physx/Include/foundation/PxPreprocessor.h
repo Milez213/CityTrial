4ef356d62e37e9dd01ad3ccacefaf3102dea9566
Eric Johnson <ejohns32@calpoly.edu>
2013-01-31 00:20:23 -0800

    disable assert for linux causing issues

--- physx/Include/foundation/PxPreprocessor.h
+++ physx/Include/foundation/PxPreprocessor.h
@@ -333,7 +333,7 @@ General defines
 
 struct PxPackValidation { char _; long long a; };
 
-#if !defined(PX_APPLE_IOS)
+#if !(defined(PX_APPLE_IOS) || defined PX_LINUX )
 PX_COMPILE_TIME_ASSERT(PX_OFFSET_OF(PxPackValidation, a) == 8);
 #endif
 
