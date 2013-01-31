b174d9b86516ad9fd1be1081c7474030171411bb
Eric Johnson <ejohns32@calpoly.edu>
2013-01-31 00:21:17 -0800

    filled in physicsActor getters and setters

--- GamePhysicsActor.cpp
+++ GamePhysicsActor.cpp
@@ -6,15 +6,30 @@
 //
 //
 
+#include <iostream>
 #include "GamePhysicsActor.h"
 
 #include "GameUtilities.h"
 #include "foundation/PxTransform.h"
 
-float GamePhysicsActor::speed() { return 0.0; }
-void GamePhysicsActor::setSpeed(float s) {}
-glm::vec3 GamePhysicsActor::direction() {return glm::vec3();}
-void GamePhysicsActor::setDirection(glm::vec3 vel) {}
+float GamePhysicsActor::speed()
+{
+   return glm::length(velocity());
+}
+void GamePhysicsActor::setSpeed(float s)
+{
+   std::cerr << "setSpeedBegin\n";
+   setVelocity(direction() * s);
+   std::cerr << "setSpeedEnd\n";
+}
+glm::vec3 GamePhysicsActor::direction()
+{
+   return glm::normalize(velocity());
+}
+void GamePhysicsActor::setDirection(glm::vec3 vel)
+{
+   setVelocity(glm::normalize(vel) * speed());
+}
 
 glm::vec3 GamePhysicsActor::position() {
    return convert(mActor->getGlobalPose().p);
@@ -27,9 +42,9 @@ void GamePhysicsActor::setPosition(glm::vec3 pos)
 }
 glm::vec3 GamePhysicsActor::velocity()
 {
-   return convert(dynamic_cast<physx::PxRigidBody *>(mActor)->getLinearVelocity());
+   return convert((dynamic_cast<physx::PxRigidBody *>(mActor))->getLinearVelocity());
 }
 void GamePhysicsActor::setVelocity(glm::vec3 spd)
 {
-   dynamic_cast<physx::PxRigidBody *>(mActor)->setLinearVelocity(convert(spd));
+   (dynamic_cast<physx::PxRigidBody *>(mActor))->setLinearVelocity(convert(spd));
 }
