f9fb377dfbaa7f900cad5ce04874e6bb914f9cd7
Mustafa <mkhafate@calpoly.edu>
2013-02-12 15:09:36 -0800

    edited alpha layer in hudAtlas.bmp and made hud shader use alpha values. Fixes white line around texture from clamping

--- shaders/hud_frag.glsl
+++ shaders/hud_frag.glsl
@@ -8,10 +8,18 @@ varying vec2 vTexture;
 void main(void) {
    vec4 color;
    color = texture2D(uTexUnit, vTexture);
+
+   /*
    if (color.r < 0.9 || color.g < 0.9 || color.b < 0.9)
       gl_FragColor = vec4(color.rgb, 1.0);
+   
    else
       gl_FragColor = vec4(1.0, 1.0, 1.0, 0.0);
+
+   */
+   
+   // use alpha value in texture
+   gl_FragColor = color;
 }
 
 
