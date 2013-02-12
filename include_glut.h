74cbd43ef91f0ca65a236e4a7e7b3fd1f29f9b07
Eric Johnson <ejohns32@calpoly.edu>
2013-02-11 21:28:40 -0800

    added include_glut.h and made LoadTexture.h use it so it compiles on osx

---
+++ include_glut.h
@@ -0,0 +1,21 @@
+//
+//  include_glut.h
+//  
+//
+//  Created by Eric Johnson on 2/11/13.
+//
+//
+
+#ifndef _include_glut_h
+#define _include_glut_h
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+//#include <OPENGL/gl.h>
+#endif
+#ifdef __unix__
+#include <GL/glut.h>
+#endif
+
+
+#endif
